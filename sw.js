8abf48ddbdedcd0913c9af61a7b3fcad46a34ad0
user <user@example.com>
2026-07-11 04:20:26 +0300

    fix: close style tag after splash screen keyframes - was swallowing all HTML

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var C='eg-cache-v14';
+var C='eg-cache-v15';
 var URLS=['index.html','sw.js'];
 self.addEventListener('install',function(e){
   e.waitUntil(caches.open(C).then(function(c){return c.addAll(URLS)}));
